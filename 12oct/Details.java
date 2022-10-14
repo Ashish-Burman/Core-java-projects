09e91250c152ce40ed1268ea32b9683636ade3c2
Ashish-Burman <115772534+Ashish-Burman@users.noreply.github.com>
2022-10-14 11:14:47 +0530

    Add files via upload

---
+++ 12oct/Details.java
@@ -0,0 +1,12 @@
+
+class Details
+{
+	public static void main(String args[])
+	{
+		System.out.println("First Name-"+args[0]);
+		System.out.println("Last Name-"+args[1]);
+		System.out.println("Age-"+args[2]);
+		System.out.println("Course-"+args[3]);
+		System.out.println("Grade-"+args[4]);
+	}
+}
